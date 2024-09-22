7b2b13d1068292ec6155046d5f487c13d375689a
sarveshchaudhari <chaudharisarvesh10@gmail.com>
2024-09-22 23:55:13 +0530

    written the code for attention layer and the decoder layer

--- layers.py
+++ layers.py
@@ -3,6 +3,7 @@ import tensorflow as tf
 import tensorflow_text as tf_text
 from preprocessing import *
 from shapeChecker import *
+import matplotlib.pyplot as plt
 
 #at each time step the encoder ouput is combined with decoders output
 
@@ -46,6 +47,15 @@ class Encoder(tf.keras.layers.Layer):
 
         return x
     
+    def convert_input(self, texts):
+        texts = tf.convert_to_tensor(texts)#convert inpput texts to tensors
+        if len(texts.shape) == 0: #check if texts has zero dimensions
+            texts = tf.convert_to_tensor(texts)[tf.newaxis]
+        context = self.text_processor(texts).to_tensor()
+        context = self(context)#this line invokes the call method of the encoder processing the tokent Ids through the embedding and GRU layers resul is a tensor with contextual information about input sentence
+
+        return context
+    
 
 #testing
 # Encode the input sequence.
@@ -57,5 +67,128 @@ print(f'Encoder output, shape (batch, s, units): {ex_context.shape}')
 
 
 
+class CrossAttention(tf.keras.layers.Layer): #iherits fomr tf.keras.layers.Layer
+    def __init__(self, units, **kwargs): #constructor method that initialize the layer units is the dimensionality of the layer **kwargs variable input arguments (variable list of arguments)
+        super().__init__() #calling the constructor of the parent class
+        self.mha = tf.keras.layers.MultiHeadAttention(key_dim=units, num_heads=1, **kwargs) 
+        self.layernorm = tf.keras.layers.LayerNormalization() #normalization layer normalizess the features
+        self.add = tf.keras.layers.Add() #creates addition lyer which is used to add the residual connection
+
+    def call(self, x, context): #defines the forward pass of the layer it takes two input x and context the value and key from the encoder
+        shape_checker = ShapeChecker() #ensures that the shapes of the inputs the expected format. verifies the the dimensions of tensors at different steps
+
+        shape_checker(x, 'batch t units')#checks the shape of the x tensor t is time dimesion - sewuence length of the query
+        shape_checker(context, 'batch s units')#s is the sequence length of the context
+        
+        #the mha layer computes attention by comparing the query x and the context value=context
+        attn_output, attn_scores = self.mha(#attn_output is the result of the attention mechanism its a wighted sum of the context vectors, attn_scores are the attention weights that indicate how much attention wach query element gives to the context element
+            query = x,
+            value = context,
+            return_attention_scores = True#ensures that the atteetion scores are returned for future use (eg. for visualization or debugging)
+        )
+
+        shape_checker(x, 'batch t units')
+        shape_checker(attn_scores, 'batch heads t s')
+
+        #cache the attention scores for plotting layer
+        attn_scores = tf.reduce_mean(attn_scores, axis=1) #averaging attention scores
+        shape_checker(attn_scores, 'batch t s')
+        self.last_attention_weights = attn_scores
+
+        x = self.add([x, attn_output]) #This is the residual connection. The input x is added to the attn_output, which allows the model to retain information from the original query x while adding in the new information from the attention mechanism.
+        x = self.layernorm(x) #After the addition, layer normalization is applied to x to stabilize the training process by ensuring the output has a mean of 0 and variance of 1. This prevents exploding or vanishing gradients and improves convergence.
+
+        return x
 
 
+
+####test#####
+attention_layer = CrossAttention(UNITS)
+
+# Attend to the encoded tokens
+embed = tf.keras.layers.Embedding(target_text_processor.vocabulary_size(),output_dim=UNITS, mask_zero=True)
+ex_tar_embed = embed(ex_tar_in)
+
+result = attention_layer(ex_tar_embed, ex_context)
+
+print(f'Context sequence, shape (batch, s, units): {ex_context.shape}')
+print(f'Target sequence, shape (batch, t, units): {ex_tar_embed.shape}')
+print(f'Attention result, shape (batch, t, units): {result.shape}')
+print(f'Attention weights, shape (batch, t, s):    {attention_layer.last_attention_weights.shape}')
+
+
+#The attention weights will sum to 1 over the context sequence, at each location in the target sequence.
+attention_layer.last_attention_weights[0].numpy().sum(axis=-1)
+#attention weights across the context sequences at t = 0
+
+
+attention_weights = attention_layer.last_attention_weights
+mask=(ex_context_tok != 0).numpy()
+
+plt.subplot(1, 2, 1)
+plt.pcolormesh(mask*attention_weights[:, 0, :])
+plt.title('Attention weights')
+
+plt.subplot(1, 2, 2)
+plt.pcolormesh(mask)
+plt.title('Mask');
+
+plt.show()
+
+
+#The Decoder
+#The decoders's  is to generate predictions for the next token at each location in the target sequence.
+#at each time step by attending to the encoded input from the encoder
+#1. for each token in the target sequence(in english) the decoder looks up embeddings converting the token IDs into continuos vectors.
+#2. the decoder uses a unidorectional GRU to process these embeddings in a left to right order
+#3. at each time step the RNN output is used as the 'query' to an attention layer. the attention layer helps the decoder focus on different parts of the encoder's output (context) ot predict the next word
+#4. Using the RNN's output, the decoder predicts the next token in the target sequence by applying a fully connected layer (Dense) to generate a probability distribution over the entire vocabulary.
+
+class Decoder(tf.keras.layers.Layer):
+    #class initializer initializes all the layers
+    @classmethod
+    def add_method(cls, fun): #this is utility decorator, you can add methods dynamically to the decoder class. 
+        setattr(cls, fun.__name__, fun)#setattr function assigns the function fun as a method of the class
+        return fun
+
+
+    def __init__(self, text_processor, units):
+        super(Decoder, self).__init__()
+
+        self.text_processor = text_processor#responsible for converting raw text to tokens
+        
+        self.vocab_size = text_processor.vocab_size()
+
+        self.word_to_id = tf.keras.layers.StringLookup( #creates a stringLookup layer to map words to their corresponding token IDs
+            vocabulary = text_processor.get_vocabulary(),
+            mask_token = '',  #any word not in vocabulary 
+            oov_token = '[UNK]',#will be alotted [UNK] out-of-vocabulary
+            
+        )
+
+        self.id_to_word = tf.keras.layers.StringLookup(#created another stringloojup layer but inverts the mapping allowing you to convert token IDs back to words, helps in interpreting out models output to readable form
+            vocabulary=text_processor.get_vocabulary(),
+            mask_token = '',
+            oov_token = "[UNK]",
+            invert = True
+        )
+
+        self.start_token = self.word_to_id('[START]')
+        self.end_token = self.word_to_id('[END]')
+        self.units = units
+
+
+        self.embedding = tf.keras.layers.Embedding(self.vocab_size, units, mask_zero=True) #initializes an embedding layer that converts token IDs into dense vectors of a fixed size units, mask_zero = true means that the zero token will be ignored in computations because it is often used for padding 
+
+
+        self.rnn = tf.keras.layers.GRU(units,
+                                       return_sequences=True, #retuns hidden state output for each time step, necessary for attention mechanisms
+                                       return_state=True, #enabkes the GRU last hidden state 
+                                       recurrent_initializer='glorot_uniform')#used for initializing weights 
+        
+
+        self.attention = CrossAttention(units)#initializes an instance of the CrossAttention class, which will use the ouput from the RNN as the query when attending to the encoder's output this allows the decoder to focus on different paurs of the encoder's output at each time step
+
+        
+        self.output_layer = tf.keras.layers.Dense(self.vocab_size)#takes the putput fomr the previous layer (here GRU) and transforms it into a logits vector that corresponds to the vocabulary size each element in this logits vector represents the raw prediction score for each word in the vocabulary
+        
